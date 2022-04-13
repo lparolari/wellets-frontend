fca70b2c959fd9853c226318d42eabef5696610e
lparolari <luca.parolari23@gmail.com>
2022-04-13 21:28:15 +0200

    chore: update balance component style

---
+++ src/Components/Molecules/Balance/Balance.styles.ts
@@ -0,0 +1,9 @@
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+  balance: {
+    whiteSpace: 'nowrap',
+  },
+});
+
+export default useStyles;
---
+++ src/Components/Molecules/Balance/BalanceBadge.styles.ts
@@ -0,0 +1,9 @@
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+  balanceBadge: {
+    whiteSpace: 'nowrap',
+  },
+});
+
+export default useStyles;
